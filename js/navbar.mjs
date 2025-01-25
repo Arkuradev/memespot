1e1fd64b3eab4d252557f064b54acfa2e22a19f2
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2025-01-25 22:34:20 +0100

    Fixed navigation
    Made a template for navigation bar.

--- js/navbar.mjs
+++ js/navbar.mjs
@@ -1,30 +1,7 @@
-const loginLink = document.getElementById("loginLink");
-const token = localStorage.getItem("token");
-const themeToggle = document.getElementById("themeToggle");
-const rootElement = document.documentElement;
+//Toggle for mobile menu
+const navToggle = document.querySelector("#navToggle");
+const mobileMenu = document.querySelector("#mobileMenu");
 
-// Changes the name of the login link on the navbar depending on if the user is logged in or not.
-if (token) {
-  loginLink.textContent = "Logout";
-  loginLink.href = "#";
-  loginLink.classList.add("text-red-500"); // Highlight logout link
-  loginLink.addEventListener("click", () => {
-    localStorage.removeItem("token");
-    window.location.href = "../account/login.html";
-  });
-}
-
-// Theme toggle logic
-
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-  rootElement.classList.add("dark", savedTheme === "dark");
-  themeToggle.textContent = savedTheme === "dark" ? "🌙" : "🌞";
-}
-
-// Toggle theme on button click.
-themeToggle.addEventListener("click", () => {
-  const isDarkMode = rootElement.classList.toggle("dark");
-  localStorage.setItem("theme", isDarkMode ? "dark" : "light");
-  themeToggle.textContent = isDarkMode ? "🌙" : "🌞";
+navToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
 });
